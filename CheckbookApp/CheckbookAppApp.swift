f0a32e05bcd9aa27ae37e85719ebbf7289662c61
John Pierson <Johnmpierson@outlook.com>
2025-10-31 16:56:12 -0600

    Initial Commit

---
+++ CheckbookApp/CheckbookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckbookAppApp.swift
+//  CheckbookApp
+//
+//  Created by John Pierson on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct CheckbookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckbookApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CheckbookApp
+//
+//  Created by John Pierson on 10/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
